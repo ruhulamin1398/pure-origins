7639a3d1bb1101538ebd785901125d4d7ca74918
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-15 21:38:23 +0600

    project init

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,164 @@
+
+
+    
+          <!-- Sidebar -->
+          <ul class="navbar-nav bg-abasas-dark  sidebar sidebar-dark accordion sidebar-toggled " id="accordionSidebar">
+
+          
+            
+            <li class="nav-item  ">
+                <a class="nav-link text-center btn  " href="">
+                    
+                    <span class=" text-light   "> <i class="fas fa-plus text-light   "></i>New </span></a>
+            </li> 
+       
+          
+ <hr class="sidebar-divider m-1 p-0 ">
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li> 
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Top News</span></a>
+</li>
+ 
+ <hr class="sidebar-divider m-1 p-0 ">
+<li class="nav-item">
+    <a class="nav-link collapsed  p-3 " href="#" data-toggle="collapse" data-target="#collapseProducts" aria-expanded="true" aria-controls="collapseProducts">
+        <i class="fas fa-fw fa-cog  "></i>
+        <span>Event</span>
+    </a>
+    <div id="collapseProducts" class="collapse" aria-labelledby="headingProducts" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="">All Events</a>
+            <a class="collapse-item" href="">New Event</a>
+            <a class="collapse-item" href="">Event Category</a> 
+        </div>
+    </div>
+</li>
+<!-- Divider -->
+<hr class="sidebar-divider m-1 p-0 ">
+
+
+<li class="nav-item">
+    <a class="nav-link collapsed  p-3 " href="#" data-toggle="collapse" data-target="#collapseSell" aria-expanded="true" aria-controls="collapseSell">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Blog</span>
+    </a>
+    <div id="collapseSell" class="collapse" aria-labelledby="headingSell" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="">All Blogs </a>
+            <a class="collapse-item" href="">New Blog </a>
+            <a class="collapse-item" href="">Blog Category </a>
+
+        </div>
+    </div>
+</li>
+
+
+<!-- Divider -->
+<!-- Divider -->
+<hr class="sidebar-divider m-1 p-0 ">
+
+
+<li class="nav-item">
+    <a class="nav-link collapsed  p-3 " href="#" data-toggle="collapse" data-target="#collapseEvent" aria-expanded="true" aria-controls="collapseEvent">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Program</span>
+    </a>
+    <div id="collapseEvent" class="collapse" aria-labelledby="headingSell" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="">All Programs </a>
+            <a class="collapse-item" href="">New Program </a> 
+
+        </div>
+    </div>
+</li>
+
+
+<hr class="sidebar-divider m-1 p-0 ">
+
+<li class="nav-item">
+    <a class="nav-link collapsed  p-3 " href="#" data-toggle="collapse" data-target="#collapsePurchase" aria-expanded="true" aria-controls="collapsePurchase">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Committee</span>
+    </a>
+    <div id="collapsePurchase" class="collapse" aria-labelledby="headingPurchase" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="">Committee </a>
+            <a class="collapse-item" href="">Add Committee </a>
+            <a class="collapse-item" href="">Designation </a>
+            <a class="collapse-item" href="">Session </a> 
+
+        </div>
+    </div>
+</li>
+
+
+
+
+
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Donations</span></a>
+</li>
+
+ <hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Salat</span></a>
+</li> 
+<hr class="sidebar-divider m-1 p-0 ">
+<li class="nav-item  ">
+   <a class="nav-link p-3 " href=""> 
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Praying Time</span></a>
+</li> 
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Imam</span></a>
+</li>
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Gallery</span></a>
+</li>
+
+
+<!-- Divider -->
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+<li class="nav-item  ">
+    <a class="nav-link p-3 " href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>User </span></a>
+</li>
+<!-- Divider -->
+<hr class="sidebar-divider m-1 p-0 ">
+<!-- Nav Item - Dashboard -->
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center  d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+    </ul>
+    <!-- End of Sidebar -->
---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+this is fronetend index page 
